8ccb937381f3a5d1fd3928da9be536d54ad2173a
kevinyosia <kevinsiregar3007@gmail.com>
2026-03-31 13:22:58 +0700

    modefied user database, tambahan default points

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('phone')->nullable();
             $table->enum('role', ['customer', 'admin', 'coach', 'manajemen'])->default('customer');
+            $table->integer('points')->default(0);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
